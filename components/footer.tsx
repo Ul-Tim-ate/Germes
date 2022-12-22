0ad34bf6e7465058a1dd3f713d885fc8385d733a
Timofey Trakhanov <timohator1@mail.ru>
2022-12-22 13:12:09 +0300

    create footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,55 @@
+import Image from "next/image";
+import styles from "../styles/footer.module.sass";
+import logo from "../public/white-logo.svg";
+
 const Footer = () => {
-  return <footer>footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={`${styles.footerContainer} container`}>
+        <div className={`${styles.logo} ${styles.footerLogo}`}>
+          <Image src={logo} alt="Логотип компании" width={164} height={157} />
+          <span className={styles.logoName}>ГЕРМЕС</span>
+        </div>
+        <p className={styles.footerRights}>
+          © 2022–2023. ООО «ТПК ГЕРМЕС». <br /> Все права защищены. При
+          использовании материалов ссылка на данный сайт обязательна.
+        </p>
+        <nav className={styles.footerNav}>
+          <ul className={styles.footerNavList}>
+            <li className={styles.footerNavItem}>Главная</li>
+            <li className={styles.footerNavItem}>Продукция</li>
+            <li className={styles.footerNavItem}>Акция</li>
+            <li className={styles.footerNavItem}>Контакты</li>
+          </ul>
+        </nav>
+        <section className={styles.footerContacts}>
+          <ul className={styles.footerContactsList}>
+            <li className={styles.footerContactsItem}>
+              <span className={styles.footerContactsItemKey}>Телефон:</span>
+              <span className={styles.footerContactsItemValue}>
+                +79272711655
+              </span>
+            </li>
+            <li className={styles.footerContactsItem}>
+              <span className={styles.footerContactsItemKey}>E-mail:</span>
+              <span className={styles.footerContactsItemValue}>
+                tgkgermess@gmail.com
+              </span>
+            </li>
+            <li className={styles.footerContactsItem}>
+              <span className={styles.footerContactsItemKey}>Адрес:</span>
+              <span
+                className={`${styles.footerContactsItemValue} ${styles.footerContactsItemAdress}`}
+              >
+                433503, Ульяновская область, <br /> г. Димитровград,
+                <br /> ул. Куйбышева, <br /> зд. 30/21, помещ. 2
+              </span>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Heading from "../components/heading";
 const Main = () => {
   return (
     <div className="">
-      asdasd
+      
     </div>
   );
 };
