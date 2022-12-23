8d5ab91e0c0d9757aadd2dfdb9f73f8b6d2f2921
Timofey Trakhanov <timohator1@mail.ru>
2022-12-23 16:34:18 +0300

    create static product advan

--- components/product/product-advan.tsx
+++ components/product/product-advan.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import styles from "../../styles/product-advan.module.sass";
 
 const ProductAdvan = () => {
   return (
-    <div>ProductAdvan</div>
-  )
-}
+    <section className={styles.productAdvan}>
+      <h2 className={styles.productAdvanHeader}>Преимущества</h2>
+      <ul className={styles.productAdvanList}>
+        <ul className={styles.productAdvanListColumn}>
+          <li className={styles.productAdvanListColumnItem}>
+            Получение слоя оксидов, нитридов, карбидов, интерметаллидов
+          </li>
+          <li className={styles.productAdvanListColumnItem}>
+            Получение покрытий толщиной до 10…15 мкм
+          </li>
+          <li className={styles.productAdvanListColumnItem}>
+            Получение многослойных покрытий с высокой адгезионной стойкостью
+          </li>
+          <li className={styles.productAdvanListColumnItem}>
+            Изделие не требует дополнительной финишной операции после напыления
+          </li>
+          <li className={styles.productAdvanListColumnItem}>
+            Технология позволяет получать тонкие слои из различных материалов
+          </li>
+          <li className={styles.productAdvanListColumnItem}>
+            экологическая чистота процесса
+          </li>
+        </ul>
+        <ul className={styles.productAdvanListColumn}>
+          <li className={styles.productAdvanListColumnItem}>
+            Достаточно высокая производительность. Системы на основе
+            вакуумно-дугового испарения материалов обеспечивают скорости
+            осаждения до 2…3 мкм/мин
+          </li>
+          <li className={styles.productAdvanListColumnItem}>
+            Широкий выбор состава покрытий
+          </li>
+          <li className={styles.productAdvanListColumnItem}>
+            Адгезия покрытия к материалу подложки в 1,5…2 раза выше, чем при
+            гальваническом нанесении покрытия
+          </li>
+          <li className={styles.productAdvanListColumnItem}>
+            Высокая твердость и коррозионная стойкость напыленного слоя
+          </li>
+          <li className={styles.productAdvanListColumnItem}>
+            Отсутствие скрытой пористости
+          </li>
+        </ul>
+      </ul>
+      <button className={styles.productAdvanClose}>закрыть</button>
+    </section>
+  );
+};
 
-export default ProductAdvan
+export default ProductAdvan;
--- components/product/product.tsx
+++ components/product/product.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import styles from "../../styles/product.module.sass";
+import ProductAdvan from "./product-advan";
 import ProductNav from "./product-nav";
 import ProductSwiper from "./product-swiper";
 
@@ -9,21 +10,24 @@ const Product: FC<ProductProps> = () => {
   return (
     <section className={styles.product}>
       <div className={`container ${styles.productWrapper}`}>
-        <h1 className={styles.productHeader}>Вакуумная металлизация</h1>
-        <p className={styles.productDescription}>
-          С помощью такой технологии происходит обработка поверхностей изделий
-          путём переноса мелких металлических частиц в вакууме. Они покрывают
-          изделия плотным слоем.
-        </p>
-        <p className={styles.productDescription}>
-          Мы покрываем изделия из дерева, железа, пластмассы, да и вообще любого
-          материала.
-        </p>
-        <div className={styles.productSwiper}>
-          <ProductSwiper />
+        <div className={styles.productInfo}>
+          <h1 className={styles.productHeader}>Вакуумная металлизация</h1>
+          <p className={styles.productDescription}>
+            С помощью такой технологии происходит обработка поверхностей изделий
+            путём переноса мелких металлических частиц в вакууме. Они покрывают
+            изделия плотным слоем.
+          </p>
+          <p className={styles.productDescription}>
+            Мы покрываем изделия из дерева, железа, пластмассы, да и вообще
+            любого материала.
+          </p>
+          <div className={styles.productSwiper}>
+            <ProductSwiper />
+          </div>
+          <button className={styles.productGetPrice}>Получить прайс</button>
         </div>
-        <button className={styles.productGetPrice}>Получить прайс</button>
         <ProductNav />
+        <ProductAdvan />
       </div>
     </section>
   );
